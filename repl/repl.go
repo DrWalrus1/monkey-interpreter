eee377d94bc18c17afb8aa9eabd2364eefb773c1
Luke Lynch <luke@lukelynch.io>
2025-04-05 15:24:06 +1100

    Add REPL

---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey_interpreter/lexer"
+	"monkey_interpreter/token"
+)
+
+// REPL - (Read Eval Print Loop)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
